554e6f53a52a0da0abf1624b79c01b85b70f214e
arturo4ever <darturosanchezg@gmail.com>
2022-12-05 23:28:01 +0000

    Home page, header component, Not found page

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
+import Home from '../pages/Home';
+import NotFound from '../pages/NotFound';
 import Login from '../containers/Login';
 import RecoveryPassword from '../containers/RecoveryPassword';
 import '../styles/global.css';
@@ -8,14 +10,14 @@ import '../styles/global.css';
 const App = () => {
     return (
         <BrowserRouter>
-            <Switch>
-                <Layout>
+            <Layout>
+                <Switch>
                     <Route exact path="/" component={Home} />
                     <Route exact path="/login" component={Login}/>
                     <Route exact path="/recovery-password" component={RecoveryPassword} />
-                    <Route component= {NotFound} />
-                </Layout>
-            </Switch>
+                    <Route path="*" component= {NotFound} />
+                </Switch>
+            </Layout>
         </BrowserRouter>
     );
 }
